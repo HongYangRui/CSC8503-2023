7ed2db11424825699e10e797842371f68e202786
HongYangRui <y.hong12@newcastle.ac.uk>
2023-12-04 21:14:44 +0000

    Update Pushdown Automata

--- CSC8503/Main.cpp
+++ CSC8503/Main.cpp
@@ -181,6 +181,71 @@ void TestStateMachine() {
 
 }
 
+class PauseScreen : public PushdownState {
+	PushdownResult OnUpdate(float dt, PushdownState** newState)override {
+		if (Window::GetKeyboard()->KeyPressed(KeyCodes::U)) {
+			return PushdownResult::Pop;
+		}
+		return PushdownResult::NoChange;
+	}
+	void OnAwake() override {
+		std::cout << "Press U to unpause game!\n";
+	}
+};
+
+class GameScreen : public PushdownState {
+	PushdownResult OnUpdate(float dt, PushdownState** newState)override {
+		pauseReminder -= dt;
+		if (pauseReminder<0 ) {
+			std::cout << "Coins mined:" << coinsMined << "\n";
+			std::cout << "Press P to pause game,or F1 to return to main menu!\n";
+			pauseReminder += 1.0f;
+		}
+		if (Window::GetKeyboard()->KeyDown(KeyCodes::P)) {
+			*newState = new PauseScreen();
+			return PushdownResult::Push;
+		}
+		if (Window::GetKeyboard()->KeyDown(KeyCodes::F1)) {
+			std::cout << "Returning to main menu!\n";
+			return PushdownResult::Pop;
+		}
+		if (rand() % 7 == 0) {
+			coinsMined++;
+		}
+		return PushdownResult::NoChange;
+	};
+	void OnAwake()override {
+		std::cout << "Preparing to mine coins!\n";
+	}
+protected:;
+		 int coinsMined = 0;
+		 float pauseReminder = 1;
+};
+class IntroScreen :public PushdownState {
+	PushdownResult OnUpdate(float dt, PushdownState** newState)override {
+		if (Window::GetKeyboard()->KeyPressed(KeyCodes::SPACE)) {
+			*newState = new GameScreen();
+			return PushdownResult::Push;
+		}
+		if (Window::GetKeyboard()->KeyPressed(KeyCodes::ESCAPE)) {
+			return PushdownResult::Pop;
+		}
+		return PushdownResult::NoChange;
+	};
+	void OnAwake()override {
+		std::cout << "Welcome to a really awesome game!\n";
+		std::cout << "Press Space To Begin or escape to quit!\n";
+	}
+};
+void TestPushdownAutomata(Window* w) {
+	PushdownMachine machine(new IntroScreen());
+	while (w->UpdateWindow()) {
+		float dt = w->GetTimer().GetTimeDeltaSeconds();
+		if (!machine.Update(dt)) {
+			return;
+		}
+	}
+}
 /*
 
 The main function should look pretty familar to you!
@@ -195,7 +260,7 @@ hide or show the
 */
 int main() {
 	Window*w = Window::CreateGameWindow("CSC8503 Game technology!", 1920, 1200,true);
-
+	TestPushdownAutomata(w);
 	if (!w->HasInitialised()) {
 		return -1;
 	}	
@@ -210,7 +275,7 @@ int main() {
 	while (w->UpdateWindow() && !Window::GetKeyboard()->KeyDown(KeyCodes::ESCAPE)) {
 		float dt = w->GetTimer().GetTimeDeltaSeconds();
 		DisplayPathfinding();
-		TestBehaviourTree();
+		/*TestBehaviourTree();*/
 		if (dt > 0.1f) {
 			std::cout << "Skipping large time delta" << std::endl;
 			continue; //must have hit a breakpoint or something to have a 1 second frame time!
